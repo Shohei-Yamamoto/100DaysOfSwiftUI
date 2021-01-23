3ab7c52fc3d919286eafc9298bb762333e4494ef
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-01-23 23:06:10 +0900

    day1 1,2

---
+++ Day1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+// 1
+var str = "Hello, playground"
+str = "Goodbye"
+str = "a"
+
+// 2
+// Swift is known as a type-safe langage, every variable must be of one specific type.
+var age = 38
+var population = 8_000_000
+// str = 33 // typeが違うためできない
