b9b44b793578c4722e95095693913d33cd47770e
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-01-25 09:22:20 +0900

    Day3 1,2

---
+++ Day3/MyPlayground.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+// MARK: 1
+let firstScore = 12
+let secondScore = 4
+let total = firstScore + secondScore
+let diffference = firstScore - secondScore
+let product = firstScore * secondScore
+let divided = firstScore / secondScore
+let remainder = 13 % secondScore
+
+
+
+// MARK: 2
+// operator overloading, an operator depends on the values you use it with.
+
+let meaningOfLife = 42
+let doubleMeaning = 42 + 42
+
+let fakers = "Fakers gonna "
+let action = fakers + "fake"
+
+let firstHalf = ["John", "Paul"]
+let secondHalf = ["George", "Ringo"]
+let beatles = firstHalf + secondHalf
+
+// adding an integer to a string doesn't make any sense, swift is type-safe language
