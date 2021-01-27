782e1935a6b6a046a10e1b115c726404cfc50f08
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-01-27 20:24:06 +0900

    I finally done day5
    ;

--- Day5/MyPlayground.playground/Contents.swift
+++ Day5/MyPlayground.playground/Contents.swift
@@ -49,3 +49,56 @@ greet("Talor")
 
 // generally, it's better to give your parameters external names to avoid confusion
 // ex: setAlarm(5) does it set an alarm for 5 o'clock or set an alarm for five hours from now?
+
+// MARK: 6
+func greet(_ person: String, nicely: Bool = true) {
+    if nicely == true {
+        print("Hello, \(person)!")
+    } else {
+        print("Oh no, it's \(person) again...")
+    }
+}
+
+
+// MARK: 7
+func square(numbers: Int...) {
+    // numbers is Array<Int>
+    for number in numbers {
+        print("\(number) squared is \(number * number)")
+    }
+}
+
+square(numbers: 1,2,3,4)
+
+// MARK: 8
+
+// we need to define an enum that describes the errors
+enum PasswordError: Error {
+    case obvious
+}
+
+func checkPassword(_ password: String) throws -> Bool {
+    if  password == "password" {
+        throw PasswordError.obvious
+    }
+    return true
+}
+
+// MARK: 9
+do {
+    try checkPassword("password")
+    print("good!")
+} catch {
+    print("You can't use that password.")
+}
+
+// MARK: 10
+
+// if you want to change parameter variables in functions, use inout
+func doubleInPlace(number: inout Int) {
+    number * 2
+}
+
+var myNum = 10
+doubleInPlace(number: &myNum)
+
