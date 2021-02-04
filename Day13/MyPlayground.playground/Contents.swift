7de61681bd9a938ff39785129d0ab0217a88ac9f
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-02-04 20:41:49 +0900

    First half of Day13 is over

---
+++ Day13/MyPlayground.playground/Contents.swift
@@ -0,0 +1,61 @@
+import UIKit
+
+// MARk: 1
+var name = "Tim McGraw"
+name = "Romeo"
+
+let name2 = "Tim McGraw"
+// name2 = "changed"
+
+
+// MARK: 2
+var name3: String
+name3 = "Tim MacGraw"
+
+var latitude: Double
+latitude = 36.166667
+
+var longitude: Float
+longitude = -86.783333
+longitude = -186.783333
+longitude = -1286.783333
+longitude = -12386.783333
+longitude = -123486.783333
+longitude = -1234586.783333
+
+latitude = -1234586.783333
+
+
+// MARK: 3
+var a = 10
+a = a + 1
+a = a - 1
+a = a * a
+
+var names = "Romeo" + " and " + "Tim McGraw"
+
+var c = a + 2
+
+a > c
+!(a > c)
+
+// MARK: 4
+// String interpolation
+"Your name is \(name)"
+
+// MARK: 5
+var evenNumbers = [2, 4, 6, 8]
+var songs = ["Shake it Off", "You Belong with Me", "Back to December"]
+
+songs[0]
+type(of: songs)
+
+var songs2: [Any] = ["Shake it Off", "You Belong with Me", "Back to December", 3]
+
+print("\(songs2)")
+
+//evenNumbers[5] = 10
+
+// MARK: 6
+var person = ["first": "Shohei", "last": "Yamamoto", "month": "July"]
+person["month"]
