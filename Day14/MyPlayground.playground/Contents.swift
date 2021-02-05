fba36b72ac035236be5be182063be7795a701cb9
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-02-05 22:22:16 +0900

    Enum

--- Day14/MyPlayground.playground/Contents.swift
+++ Day14/MyPlayground.playground/Contents.swift
@@ -38,3 +38,25 @@ let album = albuReleased(year: 2006)
 
 
 let album2 = albuReleased(year: 10) ?? "no album"
+
+
+// MARK: enum
+enum WeatherType {
+    case sun, cloud, rain, wind(speed: Int), snow
+}
+
+func getHaterStatus(weather: WeatherType) -> String? {
+    switch weather {
+    case .wind(let speed) where speed < 10:
+        return "meer"
+    default:
+        return nil
+    }
+}
+
+getHaterStatus(weather: WeatherType.cloud)
+getHaterStatus(weather: WeatherType.wind(speed: 100))
+
+getHaterStatus(weather: WeatherType.wind(speed: 1))
+
+
