c29017b3867b5763136ea93019ebd79266d2e96c
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-02-07 20:07:15 +0900

    binding

--- Project1/WeSplit/WeSplit/ContentView.swift
+++ Project1/WeSplit/WeSplit/ContentView.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = 0
+    @State private var name = ""
     var body: some View {
-        Button("Tap Count: \(tapCount)") {
-            self.tapCount += 1
+        Form {
+            TextField("Enter your name", text: $name) // two way binding that reload if that property is changed
+            Text("Hello World")
         }
     }
 }
