43413651eb1b10d18e6d2890064e01383d4bcb8b
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-02-11 23:51:08 +0900

    Day21 stacking up buttons

--- Project2/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -14,20 +14,26 @@ struct ContentView: View {
     var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        
-        VStack(spacing: 30) {
-            VStack {
-                Text("Tap the flag of")
-                Text(countries[correctAnswer])
-            }
-            ForEach(0 ..< 3) {
-                number in
-                Button(action: {}, label: {
-                    Image(self.countries[number])
-                        .renderingMode(.original)
-                })
+        ZStack {
+            Color.blue.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                }
+                ForEach(0 ..< 3) {
+                    number in
+                    Button(action: {}, label: {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                    })
+                }
+                Spacer()
             }
         }
+        
     }
 }
 
