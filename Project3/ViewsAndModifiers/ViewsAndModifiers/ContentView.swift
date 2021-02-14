44e70771e4e5173fb2d80770ff820b5a8b9727f4
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-02-15 08:59:52 +0900

    Day24 Challenge 1

--- Project3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Project3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -8,36 +8,30 @@
 import SwiftUI
 
 
-struct GridStack<Content: View>: View {
-    let rows: Int
-    let columns: Int
-    let content: (Int, Int) -> Content
-    
-    var body: some View {
-        VStack {
-            ForEach(0..<rows, id: \.self)  { row in
-                HStack {
-                    ForEach(0..<self.columns, id: \.self) { column in
-                        self.content(row, column)
-                    }
-                }
-            }
-        }
+
+struct LargeBlueFont: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(Font.largeTitle)
+            .foregroundColor(.blue)
     }
-    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
-        self.rows = rows
-        self.columns = columns
-        self.content = content
+}
+
+extension View {
+    func largeBlueFont() -> some View {
+        self.modifier(LargeBlueFont())
     }
 }
 
 struct ContentView: View {
-    
     var body: some View {
-        GridStack(rows: 4, columns: 4) { row, col in
-            Image(systemName: "\(row * 4 + col).circle")
-            Text("R\(row) C\(col)")
+        VStack {
+            Text("a")
+                .modifier(LargeBlueFont())
+            Text("b")
+                .largeBlueFont()
         }
+            
     }
 }
 
