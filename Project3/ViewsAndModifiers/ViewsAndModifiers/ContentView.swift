5ce19edd91726953e94be25963f7c6edee18bac9
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-02-14 10:30:49 +0900

    Confitional modifiers

--- Project3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Project3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -9,14 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     // opaque return types = some View
+    
+    @State private var useRedText = false
+    
     var body: some View {
         VStack {
             Text("hello")
             Button("Hello World") {
                 print(type(of: self.body))
+                useRedText = !useRedText
             }
             .frame(width: 200, height: 200)
-            .background(Color.red)
+            .background(useRedText ? Color.red : Color.blue)
         }
         
 
