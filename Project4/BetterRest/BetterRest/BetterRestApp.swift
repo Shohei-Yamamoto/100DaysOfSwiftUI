b71571fdcd323035c9f9435bcaceb974bd71f31f
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-02-17 20:36:03 +0900

    Day26 starts

---
+++ Project4/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Shohei Yamamoto on 2021/02/17.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
