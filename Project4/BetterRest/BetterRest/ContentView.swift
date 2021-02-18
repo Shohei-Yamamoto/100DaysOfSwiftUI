e98ace8c644ff48c05e493de41962cc518773a43
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-02-18 23:04:07 +0900

    Working with dates

--- Project4/BetterRest/BetterRest/ContentView.swift
+++ Project4/BetterRest/BetterRest/ContentView.swift
@@ -10,8 +10,24 @@ import SwiftUI
 struct ContentView: View {
     @State private var wakeUp = Date()
     
+
     var body: some View {
-        Form {
+        let now = Date()
+        let tomorrow = Date().addingTimeInterval(60 * 60 * 24)
+        
+        var components = DateComponents()
+        components.hour = 8
+        components.minute = 0
+        let date =  Calendar.current.date(from: components) ?? Date()
+        
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        let dateString = formatter.string(from: date)
+        
+        
+        
+        return Form {
+            Text("\(dateString)")
             DatePicker("Please enter a date", selection: $wakeUp,in: Date()...)
                 .labelsHidden()
         }
