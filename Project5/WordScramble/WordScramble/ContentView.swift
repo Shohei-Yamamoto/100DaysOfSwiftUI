b2e801b3d27c76c1ff99e5a128807dc478acf9fd
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-02-21 10:19:30 +0900

    Day29 is over

--- Project5/WordScramble/WordScramble/ContentView.swift
+++ Project5/WordScramble/WordScramble/ContentView.swift
@@ -10,9 +10,26 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        if let fileURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
-            if let fileContents = try? String(contentsOf: fileURL) {
-                Text(fileContents)
+        let input = """
+            a
+            b
+            c
+            """
+        let letters = input.components(separatedBy: "\n")
+        let letter = letters.randomElement()
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        
+        let word = "swift"
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        let allGood = misspelledRange.location == NSNotFound
+        
+        VStack {
+            ForEach(letters, id: \.self) {
+                Text("\($0)")
             }
         }
     }
