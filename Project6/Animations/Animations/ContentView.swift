5851f3602ed8d5315a4af14448e80945405c39b0
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-02-24 23:30:21 +0900

    Implicit Animations

--- Project6/Animations/Animations/ContentView.swift
+++ Project6/Animations/Animations/ContentView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount:CGFloat = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Tap me") {
+            self.animationAmount += 1
+            
+        }
+        .padding(50)
+        .foregroundColor(.white)
+        .background(Color.red)
+        .clipShape(Circle())
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
+        .animation(.default)
     }
 }
 
