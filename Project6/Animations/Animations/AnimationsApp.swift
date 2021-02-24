43b2201ca4c8839ff9267d4b4d2c9a618fd5c18a
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-02-24 21:39:41 +0900

    add project 6

---
+++ Project6/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Shohei Yamamoto on 2021/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
