1d253a49e93a9d2549f17d9fc81b4c5d5f0e36f2
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-02-28 15:40:36 +0900

    Day33 is over

--- Project6/Animations/Animations/ContentView.swift
+++ Project6/Animations/Animations/ContentView.swift
@@ -7,33 +7,42 @@
 
 import SwiftUI
 
+
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading), identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
 struct ContentView: View {
-    let letters = Array("Hello SwiftUI")
-    @State private var enabled = false
-    @State private var dragAmount = CGSize.zero
+    @State private var isShowingRed = false
+    
     
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count) {num in
-                Text(String(self.letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(self.enabled ? Color.blue : Color.red)
-                    .offset(self.dragAmount)
-                    .animation(Animation.default.delay(Double(num) / 20))
+        VStack {
+            Button("Tap me") {
+                withAnimation {
+                    self.isShowingRed.toggle()
+                }
+                
+            }
+            if isShowingRed {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
             }
         }
-            .gesture(DragGesture()
-                        .onChanged{
-                            self.dragAmount = $0.translation
-                        }
-                        .onEnded { _ in
-                            withAnimation(.spring()) {
-
-                                self.dragAmount = .zero
-                                self.enabled.toggle()
-                            }
-                        })
     }
 }
 
