7b7fe35e464db9b5de95067503b6ca9010e6e208
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-03-01 22:36:59 +0900

    day34 is over

--- Project2/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -16,6 +16,9 @@ struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var score = 0
+    @State private var rotateAngle: Angle = .zero
+    @State private var wrongOpacity: Double = 1.0
+    @State private var wrongAngle: Angle = .zero
     
     var body: some View {
         ZStack {
@@ -32,6 +35,15 @@ struct ContentView: View {
                 }
                 ForEach(0 ..< 3) {
                     FlagImage(country: self.countries[$0], number: $0, action: self.flagTapped(_:))
+                        .opacity(correctAnswer != $0 ? wrongOpacity : 1.0)
+                        .rotation3DEffect(
+                            correctAnswer != $0 ? wrongAngle : .zero,
+                            axis: (x: 0.0, y: 0.0, z: 1.0))
+                        .rotation3DEffect(
+                            rotateAngle,
+                            axis: (x: 0.0, y: 1.0, z: 0.0))
+                        .animation(correctAnswer == $0 ? .default: nil)
+                        
                 }
                 Text("Your score is \(score) now")
                     .foregroundColor(.white)
@@ -50,15 +62,24 @@ struct ContentView: View {
         if number == correctAnswer {
             scoreTitle = "Correct"
             score += 1
+            withAnimation {
+                rotateAngle.degrees += 360
+            }
         } else {
             scoreTitle = "Wrong! That's the flag of \(countries[number])"
+            wrongAngle.degrees += 30
         }
+        wrongOpacity = 0.25
         showingScore = true
     }
     
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        wrongOpacity = 1.0
+        wrongAngle = .zero
+
+        
     }
 }
 
