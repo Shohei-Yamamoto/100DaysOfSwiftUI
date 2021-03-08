f5d72d2ed4baf9e4f4aa7d719b6e3d5e2b5a720f
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-03-08 23:28:51 +0900

    Resize image

---
+++ Project8/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Shohei Yamamoto on 2021/03/08.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
