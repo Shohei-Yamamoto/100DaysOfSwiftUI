3b31f337803f6ed847a3cb7a407d914c18b05df4
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-03-09 19:58:29 +0900

    Loading specific kind of Codable Data

---
+++ Project8/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Shohei Yamamoto on 2021/03/09.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
--- Project8/Moonshot/Moonshot/ContentView.swift
+++ Project8/Moonshot/Moonshot/ContentView.swift
@@ -7,39 +7,13 @@
 
 import SwiftUI
 
-struct User: Codable {
-    var name: String
-    var address: Address
-}
-
-struct Address: Codable {
-    var street: String
-    var city: String
-}
-
-
 
 struct ContentView: View {
+    let astronauts = Bundle.main.decode("astronauts.json")
+    
     var body: some View {
-        Button("Decode JSON") {
-            let input = """
-            {
-                "name": "Taylor Swift",
-                "address": {
-                    "street": "555, Taylor Swift Avenue",
-                    "city": "Nashville"
-                }
-            }
-            """
-
-            let data = Data(input.utf8)
-            let decoder  = JSONDecoder()
-            if let user = try? decoder.decode(User.self, from: data) {
-                print(user.address.street)
-            }
-        }
         
-        
+        Text("\(astronauts.count)")
     }
 }
 
