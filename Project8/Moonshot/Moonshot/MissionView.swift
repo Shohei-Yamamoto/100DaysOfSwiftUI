b958a96cfb33862b6cda1960d4bbba6e0d4c7756
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-03-10 23:20:55 +0900

    Showing mission details

---
+++ Project8/Moonshot/Moonshot/MissionView.swift
@@ -0,0 +1,38 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Shohei Yamamoto on 2021/03/10.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    let mission: Mission
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView(.vertical) {
+                VStack {
+                    Image(self.mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: geometry.size.width * 0.7)
+                        .padding(.top)
+                    Text(self.mission.description)
+                        .padding()
+                    
+                    Spacer(minLength: 25)
+                }
+            }
+        }
+        .navigationBarTitle(Text(mission.displayName), displayMode: .inline)
+    }
+}
+
+struct MissionView_Previews: PreviewProvider {
+    static let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    static var previews: some View {
+        MissionView(mission: missions[0])
+    }
+}
