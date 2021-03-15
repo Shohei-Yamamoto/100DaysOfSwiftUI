5809f9214cc45419bd58476ce316e4893177e894
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-03-15 21:30:35 +0900

    Day 44

--- Project9/Drawing/Drawing/ContentView.swift
+++ Project9/Drawing/Drawing/ContentView.swift
@@ -8,75 +8,41 @@
 import SwiftUI
 
 
-struct Triangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-        return path
-    }
-}
-
-struct Arc: InsettableShape {
-    var startAngle: Angle
-    var endAngle: Angle
-    var clockwise: Bool
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
     
-    var insetAmount: CGFloat = 0
-    
-    func inset(by amount: CGFloat) -> some InsettableShape {
-        var arc = self
-        arc.insetAmount += amount
-        return arc
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) {value in
+                Circle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [self.color(for: value, brightness: 1), self.color(for: value, brightness: 0.5)]), startPoint: .leading, endPoint: .trailing), lineWidth: 2)
+            }
+        }.drawingGroup()
     }
     
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        let rotationAdjustment = Angle.degrees(90)
-        let modifiedStart = startAngle - rotationAdjustment
-        let modifiedEnd = endAngle - rotationAdjustment
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
         
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width/2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: clockwise)
-        return path
-    }
-}
-
-struct Flower: Shape {
-    
-    var petalOffset: Double = -20
-    var petalWidth: Double = 100
-    
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
-            let rotation = CGAffineTransform(rotationAngle: number)
-            
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width/2, y: rect.height/2))
-            
-            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width/2))
-            
-            let rotatedPetal = originalPetal.applying(position)
-            
-            path.addPath(rotatedPetal)
+        if targetHue > 1 {
+            targetHue -= 1
         }
         
-        return path
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
 
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
+    @State private var colorCycle = 0.0
     
     var body: some View {
-        Capsule()
-            .border(ImagePaint(image: Image("Example"), scale: 0.1), width: 20)
-            .frame(width: 300, height: 200)
+        VStack {
+            ColorCyclingCircle(amount: self.colorCycle)
+                .frame(width: 300, height: 300)
+            
+            Slider(value: $colorCycle)
+        }
 
     }
 }
