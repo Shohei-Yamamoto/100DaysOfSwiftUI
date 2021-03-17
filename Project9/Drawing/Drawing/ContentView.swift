8fe72e9003682ed48bd9b478813958502baa1f42
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-03-17 19:08:37 +0900

    AnimatableData

--- Project9/Drawing/Drawing/ContentView.swift
+++ Project9/Drawing/Drawing/ContentView.swift
@@ -7,47 +7,38 @@
 
 import SwiftUI
 
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+    
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { self.insetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        
+        return path
+    }
+}
 
 struct ContentView: View {
-    @State private var amount: CGFloat = 0.0
+    @State private var amount: CGFloat = 50
     
     var body: some View {
-        VStack {
-            ZStack {
-                Image("Example")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200, alignment: .center)
-                    .saturation(Double(amount))
-                    .blur(radius: (1-amount) * 20)
-                
-                Circle()
-                    .fill(Color.red)
-                    .frame(width: 200 * amount)
-                    .offset(x: -50, y: -80)
-                    .blendMode(.screen)
-                
-                Circle()
-                    .fill(Color.green)
-                    .frame(width: 200 * amount)
-                    .offset(x: 50 ,y: -80)
-                    .blendMode(.screen)
-                
-                Circle()
-                    .fill(Color.blue)
-                    .frame(width: 200 * amount)
-                    .blendMode(.screen)
-                    
+        Trapezoid(insetAmount: amount)
+            .frame(width: 200, height: 100)
+            .foregroundColor(.red)
+            .onTapGesture {
+                withAnimation {
+                    amount = CGFloat.random(in: 10...90)
+                }
             }
-            .frame(width: 300, height: 300)
-            
-            Slider(value: $amount)
-                .padding()
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.black)
-        .edgesIgnoringSafeArea(.all)
-        
     
     }
 }
