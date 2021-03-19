4b3bf54c90a7363d841ec4f12be3928dcbd35f4a
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-03-19 09:28:08 +0900

    Day45

--- Project9/Drawing/Drawing/ContentView.swift
+++ Project9/Drawing/Drawing/ContentView.swift
@@ -7,76 +7,89 @@
 
 import SwiftUI
 
-struct Trapezoid: Shape {
-    var insetAmount: CGFloat
+struct Spirograph: Shape {
+    let innerRadius: Int
+    let outerRadius: Int
+    let distance: Int
+    let amount: CGFloat
     
-    var animatableData: CGFloat {
-        get { insetAmount }
-        set { self.insetAmount = newValue }
+    func gcd(_ a: Int, _ b: Int) -> Int {
+        var a = a
+        var b = b
+        
+        while b != 0 {
+            let temp = b
+            b = a % b
+            a = temp
+        }
+        
+        return a
     }
     
     func path(in rect: CGRect) -> Path {
-        var path = Path()
-        path.move(to: CGPoint(x: 0, y: rect.maxY))
-        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        let divider = gcd(innerRadius, outerRadius)
+        let outerRadius = CGFloat(self.outerRadius)
+        let innerRadius = CGFloat(self.innerRadius)
+        let distance = CGFloat(self.distance)
+        let difference = innerRadius - outerRadius
+        let endPoint = ceil(2*CGFloat.pi * outerRadius / CGFloat(divider) * amount)
         
-        return path
-    }
-}
-
-struct Checkerboard: Shape {
-    var rows: Int
-    var columns: Int
-    
-    public var animatableData: AnimatablePair<Double, Double> {
-        get {
-            AnimatablePair(Double(rows), Double(columns))
-        }
-        
-        set {
-            self.rows = Int(newValue.first)
-            self.columns = Int(newValue.second)
-        }
-    }
-    
-    func path(in rect: CGRect) -> Path {
         var path = Path()
         
-        let rowSize = rect.height / CGFloat(rows)
-        let columnSize = rect.width / CGFloat(columns)
-        
-        
-        for row in 0..<rows {
-            for column in 0..<columns {
-                if (row + column).isMultiple(of: 2) {
-                    let startX = columnSize * CGFloat(column)
-                    let startY = rowSize * CGFloat(row)
-                    
-                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
-                    path.addRect(rect)
-                }
+        for theta in stride(from: 0, to: endPoint, by: 0.01){
+            var x = difference + cos(theta) + distance * cos(difference/outerRadius * theta)
+            var y = difference * sin(theta) - distance * sin(difference/outerRadius * theta)
+            
+            x += rect.width/2
+            y += rect.height/2
+            
+            if theta == 0 {
+                path.move(to: CGPoint(x:x, y:y))
+            } else {
+                path.addLine(to: CGPoint(x:x, y:y))
             }
         }
+        
         return path
     }
 }
-
 struct ContentView: View {
-    @State private var rows = 4
-    @State private var columns = 4
+    @State private var innerRadius = 125.0
+    @State private var outerRadius = 75.0
+    @State private var distance = 25.0
+    @State private var amount: CGFloat = 1.0
+    @State private var hue = 0.6
     
     var body: some View {
-        Checkerboard(rows: rows, columns: columns)
-            .onTapGesture {
-                withAnimation(.linear(duration: 3)) {
-                    self.rows = 8
-                    self.columns = 16
-                }
-            }
-    
+        VStack(spacing: 0) {
+            Spacer()
+            Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
+                .stroke(Color(hue: hue, saturation: 1, brightness:  1), lineWidth: 1)
+                .frame(width: 300, height: 300)
+            
+            Spacer()
+            
+            Group {
+                Text("Inner radius: \(Int(innerRadius))")
+                Slider(value: $innerRadius, in: 10...150, step: 1)
+                    .padding([.horizontal, .bottom])
+
+                Text("Outer radius: \(Int(outerRadius))")
+                Slider(value: $outerRadius, in: 10...150, step: 1)
+                    .padding([.horizontal, .bottom])
+
+                Text("Distance: \(Int(distance))")
+                Slider(value: $distance, in: 1...150, step: 1)
+                    .padding([.horizontal, .bottom])
+
+                Text("Amount: \(amount, specifier: "%.2f")")
+                Slider(value: $amount)
+                    .padding([.horizontal, .bottom])
+
+                Text("Color")
+                Slider(value: $hue)
+                    .padding(.horizontal)            }
+        }
     }
 }
 
