7345d705d519e87b8b5f20eb6f87706ddc01111a
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-03-30 23:41:41 +0900

    URLSession

--- Project10/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ Project10/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,28 +7,48 @@
 
 import SwiftUI
 
-class User: ObservableObject, Codable {
-    enum CodingKeys: CodingKey {
-        case name
-    }
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-    }
-    
-    @Published var name = "Paul Hadson"
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
 }
 
 struct ContentView: View {
+    @State private var results = [Result]()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading, content: {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            })
+        }
+        .onAppear{
+            loadData()
+        }
+        
+    }
+    
+    func loadData() {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return }
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedZResponse = try? JSONDecoder().decode(Response.self, from: data){
+                    DispatchQueue.main.async {
+                        self.results = decodedZResponse.results
+                    }
+                    return
+                }
+            }
+        }.resume()
         
     }
 }
