e6eae2aa85b659075ad9319a33e443f64d36f826
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-03-31 22:43:50 +0900

    Day49

--- Project10/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ Project10/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,50 +7,29 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
 
 struct ContentView: View {
-    @State private var results = [Result]()
+    @State private var username = ""
+    @State private var email = ""
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading, content: {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-            })
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
+            
+            Section {
+                Button("Create Account") {
+                    print("Creating account...")
+                }
+            }
+            .disabled(username.isEmpty || email.isEmpty)
         }
-        .onAppear{
-            loadData()
-        }
+        
         
     }
     
-    func loadData() {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return }
-        let request = URLRequest(url: url)
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data {
-                if let decodedZResponse = try? JSONDecoder().decode(Response.self, from: data){
-                    DispatchQueue.main.async {
-                        self.results = decodedZResponse.results
-                    }
-                    return
-                }
-            }
-        }.resume()
-        
-    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
