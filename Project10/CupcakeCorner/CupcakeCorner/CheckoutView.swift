711f06ab2c2432b65ebb339cde2ab0efd801a760
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-04-01 23:12:29 +0900

    Dat50

--- Project10/CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ Project10/CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -10,8 +10,22 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            VStack {
+                Image("cupcakes")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: geo.size.width)
+                Text("Your total is $\(self.order.cost, specifier: "%.2f")")
+                    .font(.title)
+                Button("Place Order") {
+                    
+                }.padding()
+            }
+        }
+        .navigationBarTitle("Check out", displayMode: .inline)
     }
+    
 }
 
 struct CheckoutView_Previews: PreviewProvider {
