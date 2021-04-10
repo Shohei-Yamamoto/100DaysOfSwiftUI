29f5461ce9e5a5fc3aba5ab9e0e5ee505a171be7
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-04-10 23:03:11 +0900

    Day53

--- Project11/Bookworm/Bookworm/CoreDataView.swift
+++ Project11/Bookworm/Bookworm/CoreDataView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct CoreDataView: View {
+    @Environment(\.managedObjectContext) var moc
+
     @FetchRequest(entity: Student.entity(), sortDescriptors: []) var students: FetchedResults<Student>
     
     var body: some View {
@@ -15,6 +17,20 @@ struct CoreDataView: View {
             List {
                 ForEach(students, id: \.id) { student in
                     Text(student.name ?? "Unknown")
+                }
+                Button("Add") {
+                    let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+                    let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+
+                    let chosenFirstName = firstNames.randomElement()!
+                    let chosenLastName = lastNames.randomElement()!
+                    
+                    let student = Student(context: self.moc)
+                    student.id = UUID()
+                    student.name = "\(chosenFirstName) \(chosenLastName)"
+                    
+                    try? self.moc.save()
+
                 }
             }
         }
